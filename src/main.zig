933600ab6e3712d8e12a50d577850c2e96f0e7c7
Manuel Floruß <manuel.floruss@gmail.com>
2022-11-20 09:26:27 +0100

    Update code to compile with zig 0.11.0-dev.230+fca776f8f and allow more keys.

--- src/main.zig
+++ src/main.zig
@@ -22,9 +22,10 @@ const HHOOK = HANDLE;
 const HOOKPROC = fn (nCode: c_int, wParam: WPARAM, lParam: LPARAM) callconv(WINAPI) LRESULT;
 
 extern "kernel32" fn GetConsoleWindow() callconv(WINAPI) ?HWND;
-extern "user32" fn SetWindowsHookExW(idHook: c_int, lpfn: HOOKPROC, hmod: ?HMODULE, dwThreadId: DWORD) callconv(WINAPI) ?HHOOK;
+extern "user32" fn SetWindowsHookExW(idHook: c_int, lpfn: *const HOOKPROC, hmod: ?HMODULE, dwThreadId: DWORD) callconv(WINAPI) ?HHOOK;
 extern "user32" fn UnhookWindowsHookEx(hhk: ?HHOOK) callconv(WINAPI) BOOL;
 extern "user32" fn CallNextHookEx(hhk: HHOOK, nCode: c_int, wParam: WPARAM, lParam: LPARAM) callconv(WINAPI) LRESULT;
+extern "user32" fn GetKeyState(nVirtKey: c_int) callconv(WINAPI) win.SHORT;
 
 const KBDLLHOOKSTRUCT = extern struct {
     vkCode: DWORD,
@@ -37,7 +38,7 @@ const KBDLLHOOKSTRUCT = extern struct {
 var global_hook: HHOOK = INVALID_HANDLE_VALUE;
 
 pub fn main() anyerror!void {
-    if (SetWindowsHookExW(WH_KEYBOARD_LL, LowLevelKeyboardProc, win.kernel32.GetModuleHandleW(null), 0)) |hook| {
+    if (SetWindowsHookExW(WH_KEYBOARD_LL, &LowLevelKeyboardProc, win.kernel32.GetModuleHandleW(null), 0)) |hook| {
         global_hook = hook;
     }
     if (global_hook == INVALID_HANDLE_VALUE) {
@@ -49,31 +50,39 @@ pub fn main() anyerror!void {
     }
     std.log.info("hook is installed.", .{});
     defer _ = UnhookWindowsHookEx(global_hook);
-    _ = try win.user32.messageBoxW(null, W("Zum Beenden auf 'OK' drücken"), W("Kindersicherung"), win.user32.MB_OK | win.user32.MB_ICONINFORMATION);
+    _ = win.user32.MessageBoxW(null, W("Zum Beenden auf 'OK' drücken"), W("Kindersicherung"), win.user32.MB_OK | win.user32.MB_ICONINFORMATION);
 }
 
 export fn LowLevelKeyboardProc(nCode: c_int, wParam: WPARAM, lParam: LPARAM) callconv(WINAPI) LRESULT {
     if (nCode >= 0) {
         if (@intToPtr(?*KBDLLHOOKSTRUCT, @bitCast(usize, lParam))) |keyboard| {
-            std.log.debug("KB: {}", .{keyboard.*});
+            // std.log.debug("KB: {}", .{keyboard.*});
+            const ctrl = (GetKeyState(vk.CONTROL) & 0b1000000_000000) != 0;
             var allow = switch (keyboard.vkCode) {
-                vk.VK_BACK => true,
-                vk.VK_SPACE => true,
-                vk.VK_LEFT, vk.VK_UP, vk.VK_RIGHT, vk.VK_DOWN => true,
-                0x30...0x39 => true, // 0-9
-                0x41...0x5A => true, // A-Z
-                vk.VK_NUMPAD0...vk.VK_NUMPAD9 => true, // Numpad 0-9
-                vk.VK_ADD => true,
-                vk.VK_MULTIPLY => true,
-                vk.VK_SEPARATOR => true,
-                vk.VK_SUBTRACT => true,
-                vk.VK_DECIMAL => true,
-                vk.VK_DIVIDE => true,
-                vk.VK_SHIFT, vk.VK_LSHIFT, vk.VK_RSHIFT => true,
+                vk.BACK => true,
+                vk.SPACE => true,
+                vk.LEFT, vk.UP, vk.RIGHT, vk.DOWN => true,
+                vk.HOME, vk.END, vk.PRIOR, vk.NEXT => true,
+                vk.@"0"...vk.@"9" => true, // 0-9
+                vk.A...vk.Z => !ctrl, // A-Z
+                vk.NUMPAD0...vk.NUMPAD9 => true, // Numpad 0-9
+                vk.ADD => true,
+                vk.MULTIPLY => true,
+                vk.SEPARATOR => true,
+                vk.SUBTRACT => true,
+                vk.DECIMAL => true,
+                vk.DIVIDE => true,
+                vk.SHIFT, vk.LSHIFT, vk.RSHIFT => true,
+                vk.CONTROL, vk.LCONTROL, vk.RCONTROL => true,
                 0xBA...0xF5 => true, // OEM specific
-                vk.VK_OEM_CLEAR => true,
+                vk.OEM_CLEAR => true,
+                vk.RETURN => true,
+                vk.ESCAPE => true,
                 else => false,
             };
+            if (ctrl and keyboard.vkCode == vk.A) { // CTRL+A
+                allow = true;
+            }
             if (!allow) {
                 std.log.info("INTERCEPT", .{});
                 return 1;
